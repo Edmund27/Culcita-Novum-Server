ec7ed33ae64539a3aef43cefd2187e3ffd7bbeff
edmund27 <epafko@gmail.com>
2020-11-06 06:53:56 +0100

    implemented socket io chat

---
+++ methods/chat.js
@@ -0,0 +1,36 @@
+const Chat = require("../models").chat
+const Message = require("../models").message
+const { Op } = require("sequelize")
+
+
+const findOrCreateChat = async (user1Id, user2Id) => {
+    const chat = await Chat.findOne(
+        {
+            where: {
+                user1Id: {
+                    [Op.or]: [user1Id, user2Id]
+                },
+                user2Id: {
+                    [Op.or]: [user1Id, user2Id]
+                }
+            },
+            include: [Message],
+            order: [[Message, 'createdAt', 'ASC']]
+        }
+    )
+    if (chat) {
+        return chat
+    } else {
+        return (
+            await Chat.create({
+                user1Id: user1Id,
+                user2Id: user2Id
+            }, {
+                include: [Message],
+                order: [[Message, 'createdAt', 'DESC']]
+            })
+        )
+    }
+}
+
+module.exports = findOrCreateChat
